93798bdcb7ba5f0aa0a263e3a1d0922c5326231b
brokestar233 <3765589194@qq.com>
2025-09-07 09:50:59 +0800

    sched: Allow sched_class::dequeue_task() to fail
    Change the function signature of sched_class::dequeue_task() to return
    a boolean, allowing future patches to 'fail' dequeue.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Tested-by: Valentin Schneider <vschneid@redhat.com>
    Link: https://lkml.kernel.org/r/20240727105028.864630153@infradead.org

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2361,6 +2361,9 @@ struct sched_class {
 #endif
 
 	void (*enqueue_task) (struct rq *rq, struct task_struct *p, int flags);
+#ifndef __GENKSYMS__
+	bool (*__dequeue_task) (struct rq *rq, struct task_struct *p, int flags);
+#endif
 	void (*dequeue_task) (struct rq *rq, struct task_struct *p, int flags);
 	void (*yield_task)   (struct rq *rq);
 	bool (*yield_to_task)(struct rq *rq, struct task_struct *p);
@@ -2561,6 +2564,8 @@ extern void init_sched_fair_class(void);
 
 extern void reweight_task(struct task_struct *p, int prio);
 extern void __setscheduler_prio(struct task_struct *p, int prio);
+extern bool __dequeue_task(struct rq *rq, struct task_struct *p, int flags);
+extern void dequeue_task(struct rq *rq, struct task_struct *p, int flags);
 
 extern void resched_curr(struct rq *rq);
 extern void resched_cpu(int cpu);
